67b87516a2eb153b37fcc705e8896d1396b8ac97
Daniel Justiz <danieljustiz9508@gmail.com>
2019-09-10 01:16:23 -0400

    dropdown js and arrow animation

--- mobile-menu-dropdown.js
+++ mobile-menu-dropdown.js
@@ -1,15 +1,38 @@
 const dropdownButton = document.querySelectorAll('button.dropdown-button');
-const dropdownItems = document.querySelectorAll('ul.dropdown-items');
+
 
 for (let i = 0; i < dropdownButton.length; i++) {
     dropdownButton[i].addEventListener('click', () => {
-        for (let j = 0; j < dropdownItems.length; j++) {
-            if (dropdownItems[j].classList.contains('inactive')) {
-                dropdownItems[j].classList.remove('inactive');
-                dropdownItems[j].classList.add('active');
-            }
+        if (dropdownButton[i].nextElementSibling.classList.contains('inactive')) {
+            dropdownButton[i].nextElementSibling.classList.remove('inactive');
+            dropdownButton[i].nextElementSibling.classList.add('active');
+        } else if (dropdownButton[i].nextElementSibling.classList.contains('active')) {
+            dropdownButton[i].nextElementSibling.classList.remove('active');
+            dropdownButton[i].nextElementSibling.classList.add('inactive');
         }
     });
 }
+
+for (let i = 0; i < dropdownButton.length; i++) {
+    dropdownButton[i].addEventListener('click', play, false);
+    function play() {
+        dropdownButton[i].firstElementChild.nextElementSibling.className = "arrow";
+        window.requestAnimationFrame(function (time) {
+            window.requestAnimationFrame(function (time) {
+                dropdownButton[i].firstElementChild.nextElementSibling.className = "arrow rotateup";
+            });
+        });
+    }
+}
+
+// document.querySelector(".runButton").addEventListener("click", play, false);
+// function play() {
+//     document.querySelector(".box").className = "box";
+//     window.requestAnimationFrame(function (time) {
+//         window.requestAnimationFrame(function (time) {
+//             document.querySelector(".box").className = "box changing";
+//         });
+//     });
+// }
 
 
