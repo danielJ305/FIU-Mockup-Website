03e87189334f36992130334879f864cc4261146f
Daniel Justiz <danieljustiz9508@gmail.com>
2019-09-14 13:26:47 -0400

    Mobile menu tab/ swup:animationInDone JS

--- mobile-menu-dropdown.js
+++ mobile-menu-dropdown.js
@@ -1,38 +1,23 @@
-const dropdownButton = document.querySelectorAll('button.dropdown-button');
 
+document.addEventListener('swup:animationInDone', () => {
 
-for (let i = 0; i < dropdownButton.length; i++) {
-    dropdownButton[i].addEventListener('click', () => {
-        if (dropdownButton[i].nextElementSibling.classList.contains('inactive')) {
-            dropdownButton[i].nextElementSibling.classList.remove('inactive');
-            dropdownButton[i].nextElementSibling.classList.add('active');
-        } else if (dropdownButton[i].nextElementSibling.classList.contains('active')) {
-            dropdownButton[i].nextElementSibling.classList.remove('active');
-            dropdownButton[i].nextElementSibling.classList.add('inactive');
-        }
-    });
-}
+    const dropdownButton = document.querySelectorAll('button.dropdown-button');
 
-for (let i = 0; i < dropdownButton.length; i++) {
-    dropdownButton[i].addEventListener('click', play, false);
-    function play() {
-        dropdownButton[i].firstElementChild.nextElementSibling.className = "arrow";
-        window.requestAnimationFrame(function (time) {
-            window.requestAnimationFrame(function (time) {
-                dropdownButton[i].firstElementChild.nextElementSibling.className = "arrow rotateup";
-            });
+    for (let i = 0; i < dropdownButton.length; i++) {
+        dropdownButton[i].addEventListener('click', () => {
+            console.log('i')
+            if (dropdownButton[i].nextElementSibling.classList.contains('active')) {
+                dropdownButton[i].nextElementSibling.classList.remove('active');
+            } else {
+                dropdownButton[i].nextElementSibling.classList.add('active');
+            }
         });
     }
-}
 
-// document.querySelector(".runButton").addEventListener("click", play, false);
-// function play() {
-//     document.querySelector(".box").className = "box";
-//     window.requestAnimationFrame(function (time) {
-//         window.requestAnimationFrame(function (time) {
-//             document.querySelector(".box").className = "box changing";
-//         });
-//     });
-// }
+});
+
+
+
+
 
 
